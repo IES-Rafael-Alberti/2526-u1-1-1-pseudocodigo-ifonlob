85d2ace25a0a7d0851e6bb7b90559e017a621769
ifonlob <ifonlob1108@g.educaand.es>
2025-09-23 10:44:57 +0200

    Creado conversión de grados Celsius a Fahrenheit

---
+++ conversión_celsius_fahrenheit.py
@@ -0,0 +1,3 @@
+# Convierte una temperatura de Celsius a Fahrenheit.
+grados_celsius = float(input("A continuación escriba una temperatura en grados Celsius:\n"))
+print("La temperatura en Fahrenheit es" ,grados_celsius*(9/5)+32 ,"ºF")
