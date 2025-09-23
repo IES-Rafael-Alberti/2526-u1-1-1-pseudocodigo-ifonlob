6d3db60a2647ff82ccccb2eab472899dd91e5901
ifonlob <ifonlob1108@g.educaand.es>
2025-09-23 11:59:13 +0200

    Creado programa cálculo IVA

---
+++ calculo_final_iva.py
@@ -0,0 +1,16 @@
+# Calcula el precio final sumando al precio sin IVA el porcentaje de IVA indicado.
+print("Este programa calculará el precio del producto con IVA.")
+entradaValida = False
+iva=0.21
+while not entradaValida:
+    try:
+        precio_producto = float(input("Por favor, a continuación introduzca el precio del producto sin IVA:\n"))
+        if precio_producto < 0:
+            print("El precio no puede ser negativo")
+            entradaValida = False
+        else:
+            print ("El precio del producto con IVA es de:", precio_producto + iva*precio_producto, "€")
+            entradaValida = True
+    except ValueError:
+        print("Introduzca un número por favor.")
+        entradaValida = False
