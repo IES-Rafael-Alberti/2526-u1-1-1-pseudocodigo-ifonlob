e4fbde45821576edf444991db6c189f99dff8727
ifonlob <ifonlob1108@g.educaand.es>
2025-09-23 10:40:21 +0200

    Creado bienvenida usuario

---
+++ bienvenida_usuario.py
@@ -0,0 +1,10 @@
+# Solicita el nombre del usuario y muestra un saludo personalizado.
+print("Bienvenido, por favor a continuación introduzca su nombre:")
+nombre = input().capitalize();
+print(f"Hola,{nombre}")
+
+
+
+
+
+
